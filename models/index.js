bc676cbb4ce03efcffb1d83c88af7b3648e37b0f
PeterBookmyer <101828030+PeterBookmyer@users.noreply.github.com>
2022-06-14 19:34:38 -0600

    updated db models to have proper column info...also setup all model associations

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: "catagory_id",
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: "catagory_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
@@ -18,3 +34,21 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+Driver.hasOne(License, {
+  foreignKey: "driver_id",
+  onDelete: "CASCADE",
+});
+
+License.belongsTo(Driver, {
+  foreignKey: "driver_id",
+});
+
+Driver.hasMany(Car, {
+  foreignKey: "driver_id",
+  onDelete: "CASCADE",
+});
+
+Car.belongsTo(Driver, {
+  foreignKey: "driver_id",
+});
